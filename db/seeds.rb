068cbe7a0794dde4bb75f52452018170c1adaa22
Leonardo Diaz <leo.diaz.trejo@gmail.com>
2023-11-18 14:05:51 -0600

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraph,
+    poster_url: Faker::Internet.url,
+    rating: rand(0.0..10.0).round(1)
+  )
+end
